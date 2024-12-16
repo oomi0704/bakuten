88cace7eeb69c6b86b7a0a1dead47e81e40be621
OMI MATSUMOTO <omi@OMI-AIR.local>
2024-12-16 22:51:17 +0900

    config.hostsを追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Bakuten
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'https://bakuten.onrender.com'
   end
 end
